09f2d397fddc8fd47c1833cb43beb6a1b379b90f
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-10-14 13:22:43 +0200

    add the api now return the real data instead of a dummy reply.

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,12 +1,33 @@
+import dotenv from 'dotenv';
 import express, { Request, Response, NextFunction, Router } from 'express';
+
 export class ApiController {
-  getStockHistory(req: Request, res: Response, next: NextFunction) {
+  async getStockHistory(req: Request, res: Response, next: NextFunction) {
     const id = req.params.id
-    // grab the URL parameter
     const { from, to } = req.query
 
-    console.log(`You requested the stock with the ID of ${id} from ${from} to ${to}`)
+    try {
+    dotenv.config({ path: './.env' });
+    const apiKey = process.env.FINNHUB_API_KEY;
 
-    res.send(`Hello from the API controller! You requested the stock with the ID of ${id} from ${from} to ${to}`)
+    console.log(`Request: the stock with the ID of ${id} from ${from} to ${to}`)
+
+    const apiURL = `https://finnhub.io/api/v1/stock/candle?symbol=${id}&resolution=1&from=${from}&to=${to}&token=${apiKey}`;
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const response = await fetch(`${apiURL}`, options);
+    const data = await response.json();
+
+    res.send(data)
+
+    } catch (error) {
+      res.send(`Error: You requested the stock with the ID of ${id} from ${from} to ${to}, no data could be retrieved.`)
+    }
   }
 }
