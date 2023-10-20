f3901059dc814034ff09b48ce33cec27bc62a5c5
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-10-20 17:07:55 +0200

    implement api call throttle with call queue

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -10,9 +10,10 @@ export class ApiController {
     dotenv.config({ path: './.env' });
     const apiKey = process.env.FINNHUB_API_KEY;
 
+    console.log('Time: ', new Date().toLocaleString())
     console.log(`Request: the stock with the ID of ${id} from ${from} to ${to}`)
 
-    const apiURL = `https://finnhub.io/api/v1/stock/candle?symbol=${id}&resolution=1&from=${from}&to=${to}&token=${apiKey}`;
+    const apiURL = `https://finnhub.io/api/v1/stock/candle?symbol=${id}&resolution=D&from=${from}&to=${to}&token=${apiKey}`;
 
     const options = {
       method: 'GET',
@@ -24,7 +25,13 @@ export class ApiController {
     const response = await fetch(`${apiURL}`, options);
     const data = await response.json();
 
-    res.send(data)
+    const redactedData = {
+      status: data.s === 'ok' ? 'success' : 'error',
+      ticker: id,
+      timestamps: data.t,
+      closePrices: data.c,
+    }
+    res.send(redactedData)
 
     } catch (error) {
       res.send(`Error: You requested the stock with the ID of ${id} from ${from} to ${to}, no data could be retrieved.`)
--- src/serverApp.ts
+++ src/serverApp.ts
@@ -10,124 +10,182 @@ import dotenv from 'dotenv';
 import { router } from './routes/router.js'
 import cors from 'cors'
 
+class Server {
+  private server: Express | false;
+  private numberOfApiCallsLastInterval: number = 0;
+  private readonly maxApiCallsPerInterval: number = 10;
+  private apiCallsResetInterval: NodeJS.Timeout;
+  private apiCallsQueue: Array<() => void> = [];
 
-try {
-  dotenv.config();
 
-  const baseURL = process.env.BASE_URL || '/'
+  constructor() {
+    const windowSizeInSeconds = 6;
+    const windowSizeInMs = windowSizeInSeconds * 1000;
+    // Reset the number of API calls every windowSizeInMs seconds and release the queued API calls in sequence.
+    this.apiCallsResetInterval = setInterval(() => {
+      this.numberOfApiCallsLastInterval = 0;
+      // release up to maxApiCallsPerInterval queued API calls
+      const releaseQueue = this.apiCallsQueue.splice(0, this.maxApiCallsPerInterval) as Array<() => void>;
+      releaseQueue.forEach((callback) => callback());
+    }, windowSizeInMs);
+    this.server = false;
+  }
 
-  const app: Express = express();
-  const httpServer = createServer(app)
-  const port = process.env.PORT;
+  public start() {
+    try {
+      dotenv.config();
 
-  app.use(cors());
+      const baseURL = process.env.BASE_URL || '/'
 
-  if (app.get('env') === 'production') {
-    // Set up helmet to shoreup the outgoing headers.
-    app.use(helmet())
+      const app: Express = express();
+      this.server = app;
+      const httpServer = createServer(app)
+      const port = process.env.PORT;
 
-    // Allow bootstrap
-    app.use(
-      helmet.contentSecurityPolicy({
-        directives: {
-          defaultSrc: [
-            'self'
-          ],
+      app.use(cors());
 
-          scriptSrc: [
-            'self'
-          ],
+      if (app.get('env') === 'production') {
+        // Set up helmet to shoreup the outgoing headers.
+        app.use(helmet())
 
-          styleSrc: [
-            'self'
-          ],
+        // Allow bootstrap
+        app.use(
+          helmet.contentSecurityPolicy({
+            directives: {
+              defaultSrc: [
+                'self'
+              ],
 
-          imgSrc: [
-            'self'
-          ]
+              scriptSrc: [
+                'self'
+              ],
+
+              styleSrc: [
+                'self'
+              ],
+
+              imgSrc: [
+                'self'
+              ]
+            }
+          })
+        )
+      }
+
+      const limiter = rateLimit({
+        windowMs: 1 * 60 * 1000, // 1 minute
+        max: 30, // Limit each IP to 30 requests per `window`
+        standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+        legacyHeaders: false // Disable the `X-RateLimit-*` headers
+      })
+      // app.use(limiter)
+
+      const directoryFullName = dirname(fileURLToPath(import.meta.url))
+      app.use(express.static(join(directoryFullName, '..', 'dist')))
+
+      app.set('view engine', 'ejs')
+      app.set('views', join(directoryFullName, 'views'))
+      app.use(expressLayouts)
+      app.set('layout', join(directoryFullName, 'views', 'layouts', 'default'))
+
+      // Populates the request object with a body object (req.body).
+      app.use(express.urlencoded({ extended: false }))
+
+      // Setup and use session middleware (https://github.com/expressjs/session)
+      const sameSiteSetting = 'strict' as 'strict' | 'lax' | 'none' | undefined
+      const sessionOptions = {
+        name: process.env.SESSION_NAME || "DayTrader", // Don't use default session cookie name.
+        secret: process.env.SESSION_SECRET || "default", // Change it!!! The secret is used to hash the session with HMAC.
+        resave: false, // Resave even if a request is not changing the session.
+        saveUninitialized: false, // Don't save a created but not modified session.
+        cookie: {
+          maxAge: 1000 * 60 * 60 * 24, // 1 day
+          sameSite: sameSiteSetting,
+          secure: false // Set it to true in production.
+        }
+      }
+
+      if (app.get('env') === 'production') {
+        app.set('trust proxy', 1) // trust first proxy
+        sessionOptions.cookie.secure = true // serve secure cookies
+      }
+
+      app.use(session(sessionOptions))
+
+      // Middleware to count the number of API calls and queue them if they exceed the limit.
+      app.use((req, res, next) => {
+        if (req.url.startsWith('/api')) {
+          if (this.numberOfApiCallsLastInterval >= this.maxApiCallsPerInterval) {
+            this.apiCallsQueue.push(() => next());
+            console.log ("\uD83D\uDCB2 [DayTrader]\uD83D\uDCB2 -> API call queued. Number of queued API calls: " + this.apiCallsQueue.length)
+          } else {
+            this.numberOfApiCallsLastInterval++;
+            next();
+          }
+        } else {
+          next();
         }
       })
-    )
-  }
 
-  const limiter = rateLimit({
-    windowMs: 1 * 60 * 1000, // 1 minute
-    max: 30000, // Limit each IP to 30 requests per `window`
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false // Disable the `X-RateLimit-*` headers
-  })
-  app.use(limiter)
+      app.use((req, res, next) => {
+        // Pass the base URL to the views.
+        res.locals.baseURL = baseURL
+        next()
+      })
 
-  const directoryFullName = dirname(fileURLToPath(import.meta.url))
-  app.use(express.static(join(directoryFullName, '..', 'dist')))
+      app.use('/', router)
 
-  app.set('view engine', 'ejs')
-  app.set('views', join(directoryFullName, 'views'))
-  app.use(expressLayouts)
-  app.set('layout', join(directoryFullName, 'views', 'layouts', 'default'))
 
-  // Populates the request object with a body object (req.body).
-  app.use(express.urlencoded({ extended: false }))
+      app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
 
-  // Setup and use session middleware (https://github.com/expressjs/session)
-  const sameSiteSetting = 'strict' as 'strict' | 'lax' | 'none' | undefined
-  const sessionOptions = {
-    name: process.env.SESSION_NAME || "DayTrader", // Don't use default session cookie name.
-    secret: process.env.SESSION_SECRET || "default", // Change it!!! The secret is used to hash the session with HMAC.
-    resave: false, // Resave even if a request is not changing the session.
-    saveUninitialized: false, // Don't save a created but not modified session.
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24, // 1 day
-      sameSite: sameSiteSetting,
-      secure: false // Set it to true in production.
+        if (err.cause === 404) {
+          return res
+            .status(404)
+            .sendFile(join(directoryFullName, 'views', 'errors', '404.html'))
+        }
+
+        if (req.app.get('env') !== 'development') {
+          return res
+            .status(500)
+            .sendFile(join(directoryFullName, 'views', 'errors', '500.html'))
+        }
+
+        res
+          .status(Number(err.cause) || 500)
+          .render('errors/error', { error: err })
+      })
+
+
+      httpServer.listen(process.env.PORT, () => {
+        console.log(`"\uD83D\uDCB2 [DayTrader]\uD83D\uDCB2: Server is running at http://localhost:${port}`);
+      });
+
+    } catch (error: unknown) {
+
+      if (error instanceof Error) {
+        console.log("\uD83D\uDCB2 [DayTrader]\uD83D\uDCB2 -> Uncaught Exception: " + error);
+        console.error("Error Stack:", error.stack);
+      }
+      process.exitCode = 1;
     }
   }
 
-  if (app.get('env') === 'production') {
-    app.set('trust proxy', 1) // trust first proxy
-    sessionOptions.cookie.secure = true // serve secure cookies
-  }
-
-  app.use(session(sessionOptions))
-
-  app.use((req, res, next) => {
-    // Pass the base URL to the views.
-    res.locals.baseURL = baseURL
-    next()
-  })
-
-  app.use('/', router)
-
-
-  app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
-
-    if (err.cause === 404) {
-      return res
-        .status(404)
-        .sendFile(join(directoryFullName, 'views', 'errors', '404.html'))
+  async stop() {
+    if (!this.server) {
+      return;
     }
 
-    if (req.app.get('env') !== 'development') {
-      return res
-        .status(500)
-        .sendFile(join(directoryFullName, 'views', 'errors', '500.html'))
+    clearInterval(this.apiCallsResetInterval);
+
+    while (this.apiCallsQueue.length > 0) {
+      await new Promise((resolve) => {
+        setTimeout(resolve, 100);
+      });
     }
 
-    res
-      .status(Number(err.cause) || 500)
-      .render('errors/error', { error: err })
-  })
-
-
-  httpServer.listen(process.env.PORT, () => {
-    console.log(`"\uD83D\uDCB2 [DayTrader]\uD83D\uDCB2: Server is running at http://localhost:${port}`);
-  });
-
-} catch (error: unknown) {
-
-  if (error instanceof Error) {
-    console.log("\uD83D\uDCB2 [DayTrader]\uD83D\uDCB2 -> Uncaught Exception: " + error);
-    console.error("Error Stack:", error.stack);
+    this.server.listen(process.env.PORT).close();
   }
-  process.exitCode = 1;
 }
+
+const server = new Server();
+server.start();
